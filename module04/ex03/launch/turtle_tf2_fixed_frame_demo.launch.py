4f8e59378e8cdf7b433ad86a8348c1e74ef6c5f5
NekrasovaAnn <114009080+NekrasovaAnn@users.noreply.github.com>
2023-10-27 16:41:52 +0700

    add 4.3

--- module04/ex03/launch/turtle_tf2_fixed_frame_demo.launch.py
+++ module04/ex03/launch/turtle_tf2_fixed_frame_demo.launch.py
@@ -7,22 +7,25 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
+from launch_ros.actions import SetParameter
 
 
+# Создаем переменную, в которой будут хранитсься созданные узлы
 def generate_launch_description():
     demo_nodes = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('learning_tf2_py'), 'launch'),
             '/turtle_tf2_demo.launch.py']),
-       launch_arguments={'target_frame': 'carrot1'}.items(),
-       )
+        )
 
     return LaunchDescription([
         demo_nodes,
         Node(
             package='learning_tf2_py',
-            executable='dynamic_frame_tf2_broadcaster',
-            name='dynamic_broadcaster',
-            arguments=['--radius', '5', '--direction_of_rotation', '-1'],
+            executable='fixed_frame_tf2_broadcaster',
+            name='fixed_broadcaster',
+            parameters=[
+                {'delay': '0.1'}
+            ]
         ),
     ])
