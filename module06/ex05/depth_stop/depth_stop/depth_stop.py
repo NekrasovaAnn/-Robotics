5ce2397a8551a43c50aae8e4212559fa2dc703df
NekrasovaAnn <114009080+NekrasovaAnn@users.noreply.github.com>
2023-12-15 16:54:13 +0700

    d6

--- module06/ex05/depth_stop/depth_stop/depth_stop.py
+++ module06/ex05/depth_stop/depth_stop/depth_stop.py
@@ -4,6 +4,7 @@ from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge,CvBridgeError
 import cv2
+import numpy as np
     
 class Turtle(Node):
     def __init__(self):
@@ -14,21 +15,38 @@ class Turtle(Node):
         self.msg = Image()
 
     def pose_callback(self, data):
-        self.msg = data
+        bridge = CvBridge()
+        img = bridge.imgmsg_to_cv2(data,desired_encoding=data.encoding)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        self.msg = img
 
     def depth_move(self):
         message = Twist()
-        image = self.msg.data
-        if self.msg.width!=0:
-            center_color = image[int(self.msg.width*self.msg.height/2+self.msg.width/2)]
-            self.get_logger().info('dist {0} '.format(center_color))
+        image = self.msg
+        height, width, _ = image.shape
+        print(height, "   ", width)
+        if width!=0:
+            center_x, center_y = width // 2, height // 2
+            num_points = 10
+            points = []
+            for i in range(num_points):
+                angle = 2 * np.pi * i / num_points
+                x = int(center_x + 0.8 * center_x * np.cos(angle))
+                y = int(center_y + 0.8 * center_y * np.sin(angle))
+                points.append((x, y))
+            # average_point = (int(np.mean([point[0] for point in points])), int(np.mean([point[1] for point in points])))
+            pixel_values = []
+            for point in points:
+                pixel_values.append(image[point[1], point[0]])
+            average_pixel_value = np.mean(pixel_values, axis=0)
+            self.get_logger().info('dist {0} '.format(average_pixel_value))
        
-            if(center_color != 0 and center_color != 127 and center_color !=128):
+            if(average_pixel_value[0]<=1.0):
                 message.linear.x = 0.0
             else:
-                message.linear.x = 0.5
+                message.linear.x = 0.1
             message.angular.z = 0.0
-            self.publisher_.publish(message)    
+            self.publisher_.publish(message)          
 
 def main(args=None):
     rclpy.init(args=args)
